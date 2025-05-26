fb7149d2b32c169d823c3613fbfb4b058be13434
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-05-26 18:29:46 -0400

    spelling: message
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- Meta/generate-libwasm-spec-test.py
+++ Meta/generate-libwasm-spec-test.py
@@ -70,7 +70,7 @@ class AssertReturn:
 @dataclass
 class AssertTrap:
     line: int
-    messsage: str
+    message: str
     action: Action
 
 
@@ -427,7 +427,7 @@ def gen_command(command: Command, ctx: Context):
         case AssertTrap():
             if not isinstance(command.action, Invoke):
                 raise GenerateException(f"Not implemented: {type(command.action)}")
-            gen_invoke(command.line, command.action, None, ctx, fail_msg=command.messsage)
+            gen_invoke(command.line, command.action, None, ctx, fail_msg=command.message)
 
 
 def generate(description: WastDescription):
