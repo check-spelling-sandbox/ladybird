4d7ae7f2eca350e49f1cb6f2f450415179ebba63
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-05-26 08:41:21 -0400

    spelling: approximation
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -331,7 +331,7 @@ TEST_CASE(test_jpeg_sof0_several_scans_odd_number_mcu)
     TRY_OR_FAIL(expect_single_frame_of_size(*plugin_decoder, { 600, 600 }));
 }
 
-TEST_CASE(test_jpeg_sof2_successive_aproximation)
+TEST_CASE(test_jpeg_sof2_successive_approximation)
 {
     auto file = TRY_OR_FAIL(Core::MappedFile::map(TEST_INPUT("jpg/successive_approximation.jpg"sv)));
     EXPECT(Gfx::JPEGImageDecoderPlugin::sniff(file->bytes()));
